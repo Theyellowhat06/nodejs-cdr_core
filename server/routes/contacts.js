6496af5ea2d750528ed0c8a5a88488da69d54d14
Theyellowhat06 <ulaanaatb@gmail.com>
2024-03-12 17:51:51 +0800

    bank account description fixes

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -7,52 +7,95 @@ const ss = require("sqlstring");
 const key = `mq0)l2t[8G}(=gvpOP$&oc'O,i_E^<`;
 
 router.post("/updateImage", (req, res) => {
-  const {caller_id, icon} = req.body
-  console.log('update')
-  const sql = ss.format(
-    `update contacts set icon = ? where caller_id = ?`,
-    [icon, caller_id]
-  );
+  const { caller_id, icon } = req.body;
+  console.log("update");
+  const sql = ss.format(`update contacts set icon = ? where caller_id = ?`, [
+    icon,
+    caller_id,
+  ]);
   // var sql = `select id, fname, lname, username, permission from users where username = ${ss.escape(body.username)} and password = ${ss.escape(body.password)}`;
   console.log("aquery: " + sql);
   con.query(sql, async (err, result, fields) => {
-    console.log('result');
+    console.log("result");
     if (err) {
-      console.log('aa')
+      console.log("aa");
       res.json({
         success: false,
         msg: "parameter invalid",
       });
     } else {
-        res.json({
-          success: true,
-        });
+      res.json({
+        success: true,
+      });
     }
   });
 });
 
 router.post("/info", (req, res) => {
-    const {caller_id, info} = req.body
-    const sql = ss.format(
-      `update contacts set info = ? where caller_id = ?`,
-      [info, caller_id]
-    );
-    con.query(sql, async (err, result, fields) => {
-      console.log('result');
-      if (err) {
-        console.log('aa')
-        res.json({
-          success: false,
-          msg: "parameter invalid",
-        });
+  const { caller_id, info } = req.body;
+  const sql = ss.format(`update contacts set info = ? where caller_id = ?`, [
+    info,
+    caller_id,
+  ]);
+  con.query(sql, async (err, result, fields) => {
+    console.log("result");
+    if (err) {
+      console.log("aa");
+      res.json({
+        success: false,
+        msg: "parameter invalid",
+      });
+    } else {
+      res.json({
+        success: true,
+      });
+    }
+  });
+});
+
+router.post("/description", (req, res) => {
+  const { caller_id, description } = req.body;
+
+  const sql = ss.format(
+    `select * from people where account_number = ?`,
+    caller_id
+  );
+  con.query(sql, (err, result, fields) => {
+    console.log(sql);
+    if (err) {
+      res.json({
+        success: false,
+        msg: "parameter invalid",
+      });
+    } else {
+      console.log(result);
+      let sql;
+      if (result.length > 0) {
+        sql = ss.format(`update people set description = ? where id = ?`, [
+          description,
+          result[0].id,
+        ]);
       } else {
+        sql = ss.format(
+          `insert into people(description, account_number) values(?,?)`,
+          [description, caller_id]
+        );
+      }
+      con.query(sql, (err, result, fields) => {
+        if (err) {
+          res.json({
+            success: false,
+            msg: "parameter invalid",
+          });
+        } else {
           res.json({
             success: true,
           });
-      }
-    });
+        }
+      });
+    }
   });
-
+});
 
 router.post("/calls", (req, res) => {
   const body = req.body;
@@ -94,11 +137,11 @@ const formatExcelDate = (dateNumber) => {
   const dateString = `${date.getFullYear()}-${(date.getMonth() + 1)
     .toString()
     .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")} ${date
-      .getHours()
-      .toString()
-      .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
   // console.log(dateString);
-  return dateString
+  return dateString;
 };
 router.post("/excel_import", async (req, res) => {
   const body = req.body;
@@ -132,7 +175,7 @@ router.post("/excel_import", async (req, res) => {
   });
 });
 
-router.get('/calls_by_date', (req, res) => {
+router.get("/calls_by_date", (req, res) => {
   const sql = ss.format(
     "select DATE_FORMAT(Timestamp, '%Y-%m-%d %H') as formated_date, count(id) as total_call, max(Duration_s) as max_dura, min(Duration_s) as min_dura, avg(Duration_s) as avg_dura from cdr where Timestamp between ? and ? group by formated_date order by formated_date",
     [req.query.from.split("T")[0], req.query.to.split("T")[0]]
@@ -150,14 +193,14 @@ router.get('/calls_by_date', (req, res) => {
       });
     }
   });
-})
+});
 
-router.get('/calls_by_receiver', (req, res) => {
+router.get("/calls_by_receiver", (req, res) => {
   const sql = ss.format(
     "select * from cdr where Receiver_id = ? and Caller_id in (?)",
     [req.query.target, req.query.sources]
   );
-  console.log(sql)
+  console.log(sql);
   con.query(sql, (err, result, fields) => {
     if (err) {
       res.json({
@@ -171,13 +214,10 @@ router.get('/calls_by_receiver', (req, res) => {
       });
     }
   });
-})
+});
 
 router.post("/remove_all_records", async (req, res) => {
-
-  const sql = ss.format(
-    "delete from cdr where id > 0",
-  );
+  const sql = ss.format("delete from cdr where id > 0");
 
   con.query(sql, (err, result, fields) => {
     if (err) {
