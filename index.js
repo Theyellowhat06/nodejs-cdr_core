0e5654df5457c4593d102ab786bef6d599de52d8
Theyellowhat06 <ulaanaatb@gmail.com>
2024-05-16 14:41:22 +0800

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const user = require("./server/routes/user");
+const prof = require("./server/routes/prof");
+const teacher = require("./server/routes/teacher");
+const student = require("./server/routes/student");
+const schedule = require("./server/routes/schedule");
+const contacts = require("./server/routes/contacts");
+const people = require("./server/routes/people");
+const bank = require("./server/routes/bank");
+const dotenv = require("dotenv");
+const connectDB = require("./db/mongodb");
+dotenv.config();
+var cors = require("cors");
+
+// console.log(process.env)
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => res.send('Home Page Route'));
-// Add more routes as needed
+const hostname = "127.0.0.1";
+const port = 3050;
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+connectDB();
+
+app.use(express.json());
+
+app.use("/user", user);
+app.use("/bank", bank);
+app.use("/prof", prof);
+app.use("/teacher", teacher);
+app.use("/student", student);
+app.use("/schedule", schedule);
+app.use("/contacts", contacts);
+app.use("/people", people);
+
+app.listen(port);
